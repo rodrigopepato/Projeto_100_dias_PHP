bf595cfaccf05c67c48437a42b4315059c223302
rodrigopepato <rodrigo.pepato@gmail.com>
2025-02-04 23:27:56 -0300

    Dia 14: Tranformando pagina estatica em dinamica

--- php_mysql/index.php
+++ php_mysql/index.php
@@ -1,3 +1,14 @@
+<?php
+
+    require __DIR__ . '/src/conexao-bd.php';
+    require __DIR__ . '/src/Model/Produto.php';
+    require __DIR__ . '/src/Repositorio/ProdutoRepositorio.php';
+
+    $produtosRepositorio = new ProdutoRepositorio($pdo);
+    $dadosCafe = $produtosRepositorio->opcoesCafe();
+    $dadosAlmoco = $produtosRepositorio->opcoesAlmoco();
+
+?>
 <!doctype html>
 <html lang="pt-br">
 <head>
@@ -28,38 +39,16 @@
                 <img class= "ornaments" src="img/ornaments-coffee.png" alt="ornaments">
             </div>
             <div class="container-cafe-manha-produtos">
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cafe-cremoso.jpg">
+                <?php foreach($dadosCafe as $cafe): ?>
+                    <div class="container-produto">
+                        <div class="container-foto">
+                            <img src="<?= $cafe->caminhoImagem() ?>">
+                        </div>
+                        <p><?= $cafe->nome() ?></p>
+                        <p><?= $cafe->descricao() ?></p>
+                        <p><?= $cafe->precoFormatado() ?></p>
                     </div>
-                    <p>Café Cremoso</p>
-                    <p>Café cremoso irresistivelmente suave e que envolve seu paladar</p>
-                    <p>R$ 5.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cafe-com-leite.jpg">
-                    </div>
-                    <p>Café com Leite</p>
-                    <p>A harmonia perfeita do café e do leite, uma experiência reconfortante</p>
-                    <p>R$ 2.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cappuccino.jpg">
-                    </div>
-                    <p>Cappuccino</p>
-                    <p>Café suave, leite cremoso e uma pitada de sabor adocicado</p>
-                    <p>R$ 7.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cafe-gelado.jpg">
-                    </div>
-                    <p>Café Gelado</p>
-                    <p>Café gelado refrescante, adoçado e com notas sutis de baunilha ou caramelo.</p>
-                    <p>R$ 3.00</p>
-                </div>
+                <?php endforeach;?>
             </div>
         </section>
         <section class="container-almoco">
@@ -68,38 +57,16 @@
                 <img class= "ornaments" src="img/ornaments-coffee.png" alt="ornaments">
             </div>
             <div class="container-almoco-produtos">
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/bife.jpg">
+                <?php foreach ($dadosAlmoco as $almoco): ?>
+                    <div class="container-produto">
+                        <div class="container-foto">
+                            <img src="<?= $almoco->caminhoImagem() ?>">
+                        </div>
+                        <p><?= $almoco->nome() ?></p>
+                        <p><?= $almoco->descricao() ?></p>
+                        <p><?= $almoco->precoFormatado() ?></p>
                     </div>
-                    <p>Bife</p>
-                    <p>Bife, arroz com feijão e uma deliciosa batata frita</p>
-                    <p>R$ 27.90</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/prato-peixe.jpg">
-                    </div>
-                    <p>Filé de peixe</p>
-                    <p>Filé de peixe salmão assado, arroz, feijão verde e tomate.</p>
-                    <p>R$ 24.99</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/prato-frango.jpg">
-                    </div>
-                    <p>Frango</p>
-                    <p>Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante</p>
-                    <p>R$ 23.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/fettuccine.jpg">
-                    </div>
-                    <p>Fettuccine</p>
-                    <p>Prato italiano autêntico da massa do fettuccine com peito de frango grelhado</p>
-                    <p>R$ 22.50</p>
-                </div>
+                <?php endforeach; ?>
             </div>
 
         </section>
---
+++ php_mysql/src/Model/Produto.php
@@ -0,0 +1,54 @@
+<?php
+
+class Produto {
+
+    public function __construct(
+        private int $id,
+        private string $tipo,
+        private string $nome,
+        private string $descricao,
+        private string $imagem,
+        private float $preco
+    ) {}
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function tipo(): string
+    {
+        return $this->tipo;
+    }
+
+    public function nome(): string
+    {
+        return $this->nome;
+    }
+
+    public function descricao(): string
+    {
+        return $this->descricao;
+    }
+
+    public function imagem(): string
+    {
+        return $this->imagem;
+    }
+
+    public function preco(): float
+    {
+        return $this->preco;
+    }
+
+    public function precoFormatado(): string
+    {
+        return 'R$ ' . number_format($this->preco(), 2);
+    }
+
+    public function caminhoImagem(): string
+    {
+        return 'img/' . $this->imagem();
+    }
+
+}
