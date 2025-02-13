3e488d5ffb17a0776b1859db47cd09456da2e444
rodrigopepato <rodrigo.pepato@gmail.com>
2025-02-12 23:46:41 -0300

    Dia 22: Continuação MVC

--- mvc/formulario.php
+++ mvc/formulario.php
@@ -1,3 +1,21 @@
+<?php
+
+    $dbPath = __DIR__ . '/banco.sqlite';
+    $pdo = new PDO("sqlite:{$dbPath}");
+
+    $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+    $video = [
+        'url'   => '',
+        'title' => ''
+    ];
+    if ($id !== false) {
+        $statement = $pdo->prepare('SELECT * FROM videos WHERE id = ?;');
+        $statement->bindValue(1, $id, PDO::PARAM_INT);
+        $statement->execute();
+        $video = $statement->fetch(PDO::FETCH_ASSOC);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -20,7 +38,7 @@
     <header>
 
         <nav class="cabecalho">
-            <a class="logo" href="../index.html"></a>
+            <a class="logo" href="../index.php"></a>
 
             <div class="cabecalho__icones">
                 <a href="./enviar-video.html" class="cabecalho__videos"></a>
@@ -32,19 +50,31 @@
 
     <main class="container">
 
-        <form class="container__formulario" action="/novo-video.php" method="POST">
-            <h2 class="formulario__titulo">Envie um vídeo!</h3>
+        <form class="container__formulario"
+              action="<?= $id === false ? '/novo-video.php' : 'editar-video.php?id=' . $id ?>"
+              method="POST">
+            <h2 class="formulario__titulo">Envie um vídeo!</h2>
                 <div class="formulario__campo">
                     <label class="campo__etiqueta" for="url">Link embed</label>
-                    <input name="url" class="campo__escrita" required
-                        placeholder="Por exemplo: https://www.youtube.com/embed/FAY1K2aUg5g" id='url' />
+                    <input name="url"
+                           value="<?= $video['url'] ?>"
+                           class="campo__escrita"
+                           required
+                           placeholder="Por exemplo: https://www.youtube.com/embed/FAY1K2aUg5g"
+                           id='url'
+                        />
                 </div>
 
 
                 <div class="formulario__campo">
                     <label class="campo__etiqueta" for="titulo">Titulo do vídeo</label>
-                    <input name="titulo" class="campo__escrita" required placeholder="Neste campo, dê o nome do vídeo"
-                        id='titulo' />
+                    <input name="titulo"
+                           value="<?= $video['title'] ?>"
+                           class="campo__escrita"
+                           required
+                           placeholder="Neste campo, dê o nome do vídeo"
+                           id='titulo'
+                        />
                 </div>
 
                 <input class="formulario__botao" type="submit" value="Enviar" />
--- mvc/index.php
+++ mvc/index.php
@@ -1,3 +1,10 @@
+<?php
+    $dbPath = __DIR__ . '/banco.sqlite';
+    $pdo = new PDO("sqlite:{$dbPath}");
+
+    $videoList = $pdo->query('SELECT  * FROM videos;')->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -18,10 +25,10 @@
     <header>
 
         <nav class="cabecalho">
-            <a class="logo" href="./index.html"></a>
+            <a class="logo" href="./index.php"></a>
 
             <div class="cabecalho__icones">
-                <a href="./pages/enviar-video.html" class="cabecalho__videos"></a>
+                <a href="/formulario.php" class="cabecalho__videos"></a>
                 <a href="./pages/login.html" class="cabecalho__sair">Sair</a>
             </div>
         </nav>
@@ -29,62 +36,22 @@
     </header>
 
     <ul class="videos__container" alt="videos alura">
-        <li class="videos__item">
-            <iframe width="100%" height="72%" src="https://www.youtube.com/embed/pA-EgOaF23I"
-                title="YouTube video player" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img src="./img/logo.png" alt="logo canal alura">
-                <h3>Qual é o melhor hardware para programação com Mario Souto</h3>
-                <div class="acoes-video">
-                    <a href="./pages/enviar-video.html">Editar</a>
-                    <a href="./pages/enviar-video.html">Excluir</a>
+        <?php foreach ($videoList as $video): ?>
+            <li class="videos__item">
+                <iframe width="100%" height="72%" src="<?= $video['url'] ?>"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img src="./img/logo.png" alt="logo canal alura">
+                    <h2><?= $video['title'] ?></h2>
+                    <div class="acoes-video">
+                        <a href="/formulario.php?id=<?= $video['id'] ?>">Editar</a>
+                        <a href="/remover-video.php?id=<?= $video['id'] ?>">Excluir</a>
+                    </div>
                 </div>
-            </div>
-        </li>
-        <li class="videos__item">
-            <iframe width="100%" height="72%" src="https://www.youtube.com/embed/OrnUhR41MYI"
-                title="Voltando ao mercado após a maternidade: Ana Silvério" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img src="./img/logo.png" alt="logo canal alura">
-                <h3>Voltando ao mercado após a mmaternidade: Ana Silvério</h3>
-                <div class="acoes-video">
-                    <a href="./pages/enviar-video.html">Editar</a>
-                    <a href="./pages/enviar-video.html">Excluir</a>
-                </div>
-            </div>
-        </li>
-        <li class="videos__item">
-            <iframe width="100%" height="72%" src="https://www.youtube.com/embed/YhnNOTde2I0"
-                title="Mercado de Trabalho | Desmistificando Mobile - Episódio 5" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img src="./img/logo.png" alt="logo canal alura">
-                <h3>Mercado de Trabalho | Desmistificando Mobile...</h3>
-                <div class="acoes-video">
-                    <a href="./pages/enviar-video.html">Editar</a>
-                    <a href="./pages/enviar-video.html">Excluir</a>
-                </div>
-            </div>
-        </li>
-        <li class="videos__item">
-            <iframe width="100%" height="72%" src="https://www.youtube.com/embed/y8FeZMv37WU"
-                title="Conhecendo a linguagem Go | Hipsters.Talks" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img src="./img/logo.png" alt="logo canal alura">
-                <h3>Conhecendo a linguagem Go | Hipsters.Talks</h3>
-                <div class="acoes-video">
-                    <a href="./pages/enviar-video.html">Editar</a>
-                    <a href="./pages/enviar-video.html">Excluir</a>
-                </div>
-            </div>
-        </li>
+            </li>
+        <?php endforeach; ?>
     </ul>
 </body>
 
