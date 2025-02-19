21e7e3bb712dae4677fd9104b3072961fa8b3762
rodrigopepato <rodrigo.pepato@gmail.com>
2025-02-18 23:01:05 -0300

    Dia 28: Criação de routes

--- mvc/public/index.php
+++ mvc/public/index.php
@@ -3,12 +3,8 @@
 use Alura\Mvc\Repository\VideoRepository;
 use Alura\Mvc\Controller\{
     Controller,
-    DeleteVideoController,
-    EditVideoController,
-    NewVideoController,
-    VideoListController,
-    VideoFormController,
-    Error404Controller};
+    Error404Controller
+    };
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -16,24 +12,19 @@ $dbPath = __DIR__ . '/../banco.sqlite';
 $pdo = new PDO("sqlite:{$dbPath}");
 $videoRepository = new VideoRepository($pdo);
 
-if (!array_key_exists('PATH_INFO', $_SERVER) || $_SERVER['PATH_INFO'] === '/') {
-    $controller = new VideoListController($videoRepository);
-} elseif ($_SERVER['PATH_INFO'] === '/novo-video') {
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        $controller = new VideoFormController($videoRepository);
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $controller = new NewVideoController($videoRepository);
-    }
-} elseif ($_SERVER['PATH_INFO'] === '/editar-video') {
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        $controller = new VideoFormController($videoRepository);
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $controller = new EditVideoController($videoRepository);
-    }
-} elseif ($_SERVER['PATH_INFO'] === '/remover-video') {
-    $controller = new DeleteVideoController($videoRepository);
+$routes = require_once  __DIR__ . '/../config/routes.php';
+
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+
+$key = "$httpMethod|$pathInfo";
+if(array_key_exists($key, $routes)) {
+
+    $controllerClass = $routes["$httpMethod|$pathInfo"];
+
+    $controller = new $controllerClass($videoRepository);
 } else {
-    $controler = new Error404Controller();
+    $controller = new Error404Controller;
 }
 
 /** @var Controller $controler*/
