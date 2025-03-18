0f4525d3c7a3abadf9eee02e40b08a2bc1402845
rodrigopepato <rodrigo.pepato@gmail.com>
2025-03-17 23:25:29 -0300

    Dia 55: Adicionando bootstrap

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class SeriesController
 {
-    public function index(Request $request) {
+    public function index(Request $request): View {
 
         $series = [
         'Lost',
@@ -15,5 +16,9 @@ class SeriesController
         ];
 
         return view('series.index')->with('series', $series);
+    }
+
+    public function create(Request $request): View {
+        return view('series.create');
     }
 }
