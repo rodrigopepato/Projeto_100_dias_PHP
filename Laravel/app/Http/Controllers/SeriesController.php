781ed05904c8a66fb1affafedf7f8f795565be94
rodrigopepato <rodrigo.pepato@gmail.com>
2025-03-20 11:21:38 -0300

    Dia 57:  Route resource

---
+++ Laravel/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Serie;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+
+class SeriesController
+{
+    public function index(): View {
+
+        $series = Serie::query()->orderBy('nome')->get();
+
+        return view('series.index')->with('series', $series);
+    }
+
+    public function create(): View {
+        return view('series.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        Serie::create($request->all());
+
+        return to_route('series.store');
+    }
+}
