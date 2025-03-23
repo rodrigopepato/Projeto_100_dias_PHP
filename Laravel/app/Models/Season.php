24cc96625e347b470178d3a577f6b40ba93211ac
rodrigopepato <rodrigo.pepato@gmail.com>
2025-03-22 23:56:37 -0300

    Dia 60: Relacionamento entre models

---
+++ Laravel/app/Models/Season.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Season extends Model
+{
+    use HasFactory;
+
+    public function series(): BelongsTo
+    {
+        return $this->belongsTo(Serie::class);
+    }
+
+    public function episodes(): HasMany
+    {
+        return $this->hasMany(Episode::class);
+    }
+}
--- Laravel/app/Models/Serie.php
+++ Laravel/app/Models/Serie.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Serie extends Model
 {
     use HasFactory;
     protected $fillable = ['nome'];
+
+    public function temporadas(): HasMany
+    {
+        return $this->hasMany(Season::class);
+    }
 }
