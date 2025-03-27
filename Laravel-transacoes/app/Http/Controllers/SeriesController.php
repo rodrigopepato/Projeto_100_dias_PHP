a4fb418dfefd6186a98a1c8c416811de9ba6f12b
rodrigopepato <rodrigo.pepato@gmail.com>
2025-03-26 23:40:49 -0300

    Dia 64: Repository

---
+++ Laravel-transacoes/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Episode;
+use App\Models\Season;
+use App\Models\Series;
+use App\Repositories\SeriesRepository;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+
+class SeriesController
+{
+
+    public function __construct(private SeriesRepository $repository)
+    {
+    }
+
+    public function index(Request $request): View {
+
+        $series = Series::all();
+        $mensagemSucesso = $request->session()->get('mensagem.sucesso');
+
+        return view('series.index')->with('series', $series)
+            ->with('mensagemSucesso', $mensagemSucesso);
+    }
+
+    public function create(): View {
+        return view('series.create');
+    }
+
+    public function store(SeriesFormRequest $request): RedirectResponse
+    {
+        $serie = $this->repository->add($request);
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+    }
+
+
+    public function destroy(Series $series): RedirectResponse
+    {
+        $series->delete();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "A série '{$series->nome}' foi removida com sucesso.");
+    }
+
+    public function edit(Series $series)
+    {
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Series $series, SeriesFormRequest $request): RedirectResponse
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' editada com sucesso");
+    }
+}
---
+++ Laravel-transacoes/resources/views/components/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title }}</title>
+    @vite(['resources/css/app.scss'])
+</head>
+<body>
+<div class="container">
+    <h1><a href="{{ route('series.index') }}">{{ $title }}</a></h1>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    {{ $slot }}
+</div>
+</body>
+</html>
